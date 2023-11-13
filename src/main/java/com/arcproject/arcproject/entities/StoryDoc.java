ba0622caeea7cf113a408726065f937007ccb863
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-13 09:45:38 -0600

    created story document class

---
+++ src/main/java/com/arcproject/arcproject/entities/StoryDoc.java
@@ -0,0 +1,10 @@
+package com.arcproject.arcproject.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
+public class StoryDoc {
+    @Id
+    private String id;
+}
