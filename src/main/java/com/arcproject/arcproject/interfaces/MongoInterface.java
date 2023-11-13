4845ef5bf12486c84a75b578956f37c9307cef2d
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-13 09:46:20 -0600

    created comment interface

--- src/main/java/com/arcproject/arcproject/interfaces/MongoInterface.java
+++
@@ -1,7 +0,0 @@
-package com.arcproject.arcproject.interfaces;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.arcproject.arcproject.entities.ServiceMongoDocument;
-
-public interface MongoInterface extends MongoRepository<ServiceMongoDocument, String> {
-    
-}
