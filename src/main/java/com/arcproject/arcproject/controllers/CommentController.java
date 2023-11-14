c7f153277b6f27a2ed354255aa0faf4a4722a3d7
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 06:16:30 -0600

    refactored data controller to comment controller

---
+++ src/main/java/com/arcproject/arcproject/controllers/CommentController.java
@@ -0,0 +1,34 @@
+package com.arcproject.arcproject.controllers;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arcproject.arcproject.entities.CommentDoc;
+import com.arcproject.arcproject.service.CommentService;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private final CommentService commentService;
+    
+    @Autowired
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/get-by-userid/{authorId}")
+    public List<CommentDoc> sendDataToBrowser(@PathVariable String authorId) {
+        List<CommentDoc> comment = commentService.findCommentsByAuthorId(authorId);
+
+        if(comment != null){
+            return comment;
+        } else {
+            return null;
+        }
+    }
+}
