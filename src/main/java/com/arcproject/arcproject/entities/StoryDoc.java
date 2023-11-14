a426c12ba24a1be66290f69c83591bb3d7d37c35
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 11:15:03 -0600

    expanded story document class

--- src/main/java/com/arcproject/arcproject/entities/StoryDoc.java
+++ src/main/java/com/arcproject/arcproject/entities/StoryDoc.java
@@ -3,8 +3,57 @@ package com.arcproject.arcproject.entities;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="users")
+
+
+@Document(collection="stories")
 public class StoryDoc {
     @Id
     private String id;
+    private String title;
+    private String story;
+    private String date;
+    private String author;
+    private String author_uuid;
+
+
+    public StoryDoc(){
+
+    }
+
+    public String getAuthorUuid(){
+        return this.author_uuid;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getStory(){
+        return this.story;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public void setAuthorName(String author){
+        this.author = author;
+    }
+
+    // public String getAuthor(){
+    //     UserDoc user = userService.findByUuid(this.author_uuid);
+
+    //     if(user != null){
+    //         return user.getfirst_name();
+    //     } else {
+    //         return "not found";
+    //     }
+        
+    // }
+
+    public String getDate(){
+        this.date = "yesterday";
+
+        return this.date;
+    }
 }
