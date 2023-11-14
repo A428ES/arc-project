e623dbddf42eff23f2f30daa9212def19b8d2e73
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 11:17:08 -0600

    updated findByUuid

--- src/main/java/com/arcproject/arcproject/service/UserService.java
+++ src/main/java/com/arcproject/arcproject/service/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
     public UserDoc getUserByEmail(String userEmail) {
         return userInterface.findByEmail(userEmail);
     }
+
+    public UserDoc findByUuid(String uuid){
+        return userInterface.findByUuid(uuid);
+    }
 }
