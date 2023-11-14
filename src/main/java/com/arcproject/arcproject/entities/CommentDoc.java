48b34aa330dd4c46a66893707a158df4401054f3
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 06:38:42 -0600

    expanded document class for comments

--- src/main/java/com/arcproject/arcproject/entities/CommentDoc.java
+++ src/main/java/com/arcproject/arcproject/entities/CommentDoc.java
@@ -2,9 +2,35 @@ package com.arcproject.arcproject.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection="users")
+@Document(collection="comments")
 public class CommentDoc {
     @Id
-    private String id;
+    private String Id;
+
+    @Field("author_uuid")
+    private String authorId;
+
+    @Field("story_uuid")
+    private String storyId;
+
+    private String comment;
+
+    public CommentDoc(){
+
+    }
+
+    public CommentDoc(String authorId){
+        this.authorId = authorId;
+    }
+
+    public String getStoryId(){
+        return this.storyId;
+    }
+
+    public String getComment(){
+        return this.comment;
+    }
+
 }
