e6b8c1d08f78dd1abd076fef87b262a03c461413
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 06:38:25 -0600

    added null check

--- src/main/java/com/arcproject/arcproject/controllers/UserController.java
+++ src/main/java/com/arcproject/arcproject/controllers/UserController.java
@@ -1,16 +1,32 @@
 package com.arcproject.arcproject.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arcproject.arcproject.entities.UserDoc;
+import com.arcproject.arcproject.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+    
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 
-    @GetMapping("/login")
-    public String sendDataToBrowser() {
-        // Your logic to generate or retrieve data
-        String data = "a login attempt!";
-        return data;
+    @GetMapping("/get-by-email/{email}")
+    public UserDoc sendDataToBrowser(@PathVariable String email) {
+        UserDoc user = userService.getUserByEmail(email);
+
+        if(user != null){
+            return user;
+        } else {
+            return null;
+        }
     }
 }
