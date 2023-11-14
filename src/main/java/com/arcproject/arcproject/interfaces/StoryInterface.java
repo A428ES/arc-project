efe6ebdadf059986177e15ebaef3a14604513f21
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 11:16:05 -0600

    updated story interface

--- src/main/java/com/arcproject/arcproject/interfaces/StoryInterface.java
+++ src/main/java/com/arcproject/arcproject/interfaces/StoryInterface.java
@@ -1,10 +1,14 @@
 package com.arcproject.arcproject.interfaces;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.arcproject.arcproject.entities.UserDoc;
+import com.arcproject.arcproject.entities.StoryDoc;
+
 
 @Repository
-public interface StoryInterface extends MongoRepository<UserDoc, String> {
-    
+public interface StoryInterface extends MongoRepository<StoryDoc, String> {
+    List<StoryDoc> findAll();
 }
+
