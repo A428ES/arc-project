47d294bba1c6b89ac101c2f95b1d8bbca1547b80
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 06:39:00 -0600

    expanded document class for user profiles

--- src/main/java/com/arcproject/arcproject/entities/UserDoc.java
+++ src/main/java/com/arcproject/arcproject/entities/UserDoc.java
@@ -2,9 +2,38 @@ package com.arcproject.arcproject.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection="users")
 public class UserDoc {
     @Id
     private String id;
+    private String email;
+
+    @Field("first_name")
+    private String firstName;
+
+    @Field("ip_address")
+    private String ipAddress;
+
+
+    public UserDoc(){
+        
+    }
+
+    public UserDoc(String email){
+        this.email = email;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getIpAddress(){
+        return this.ipAddress;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
 }
