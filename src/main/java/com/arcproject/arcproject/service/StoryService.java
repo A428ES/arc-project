742360572f8409e127fb4bdf47020430232f1333
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-14 11:16:59 -0600

    updated story service to allow for author first/last name retrieval

--- src/main/java/com/arcproject/arcproject/service/StoryService.java
+++ src/main/java/com/arcproject/arcproject/service/StoryService.java
@@ -1,19 +1,52 @@
 package com.arcproject.arcproject.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
+import com.arcproject.arcproject.entities.StoryDoc;
+import com.arcproject.arcproject.entities.UserDoc;
 import com.arcproject.arcproject.interfaces.StoryInterface;
+import com.arcproject.arcproject.interfaces.UserInterface;
 
 @Service
 public class StoryService {
     private final StoryInterface storyInterface;
+    private final UserInterface userInterface;
 
     @Autowired
-    public StoryService(StoryInterface storyInterface){
+    public StoryService(StoryInterface storyInterface, UserInterface userInterface){
         this.storyInterface = storyInterface;
+        this.userInterface = userInterface;
     }
+
+    @Autowired
 
     public StoryInterface returnService(){
         return storyInterface;
+    }
+
+    public List<StoryDoc> findAll(){
+        List<StoryDoc> stories = storyInterface.findAll();
+
+        this.processStoriesAndUpdateAuthorNames(stories);
+
+        return stories;
+    }
+
+    public void processStoriesAndUpdateAuthorNames(List<StoryDoc> stories) {
+        stories.forEach(story -> {
+            UserDoc user = userInterface.findByUuid(story.getAuthorUuid());
+
+            if(user != null){
+                story.setAuthorName(user.getfirst_name());
+            } else {
+                story.setAuthorName("USER DELETED");
+            }
+
+            storyInterface.save(story);
+        });
     }
 }
