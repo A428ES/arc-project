600b54084661cad0ffb0b5266774f761a2444ad7
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-15 09:25:50 -0600

    created common tools util class

---
+++ src/main/java/com/arcproject/arcproject/util/CommonTools.java
@@ -0,0 +1,31 @@
+package com.arcproject.arcproject.util;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
+public final class CommonTools {
+    private CommonTools(){
+
+    }
+
+    public static String convertStamp(double stamp){
+        Instant instant = Instant.ofEpochSecond((long) stamp);
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, TimeZone.getDefault().toZoneId());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy");
+        
+        return dateTime.format(formatter);
+    }
+
+    public static Map<String, Object> convertResults(Object object){
+        Map<String, Object> jsonResponse = new HashMap<>();
+        jsonResponse.put("results", object);
+        
+        return jsonResponse;
+    }
+    
+}
