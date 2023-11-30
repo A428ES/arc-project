cd7c6fc4f86e8b589f07935d06e9f68cb31ff4ef
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 10:35:09 -0600

    updated security config class

--- src/main/java/com/arcproject/arcproject/SecurityConfig.java
+++ src/main/java/com/arcproject/arcproject/SecurityConfig.java
@@ -1,25 +1,50 @@
 package com.arcproject.arcproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.arcproject.arcproject.authentication.JwtRequestFilter;
+
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
 
+    
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+        .sessionManagement(sessionManagement ->
+        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .cors(customizer -> {
             })
-            .csrf(csrf -> csrf.disable()) 
+            .csrf(AbstractHttpConfigurer::disable) 
             .authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll()
-            )
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Permit all pre-flight requests
+            .requestMatchers("/user/login", "/register", "/", "/stories/search", "/comments/count", "/comments/display", "/error", "/user/check_logged_in").permitAll()
+            .anyRequest().authenticated())
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
+            
             ;
 
         return http.build();
@@ -35,5 +60,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
     }
 }
