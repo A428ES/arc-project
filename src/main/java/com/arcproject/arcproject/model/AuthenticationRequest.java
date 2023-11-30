fda43f56806702631d52100a9855a15683d1c795
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 11:48:59 -0600

    created authentication request dto

---
+++ src/main/java/com/arcproject/arcproject/model/AuthenticationRequest.java
@@ -0,0 +1,31 @@
+package com.arcproject.arcproject.model;
+
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+
+    // Default constructor for JSON parsing
+    public AuthenticationRequest() {}
+
+    public AuthenticationRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
