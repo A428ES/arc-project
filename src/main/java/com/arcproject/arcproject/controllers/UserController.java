035ada99b7f4cac0d64a512a114f1abc564b20a6
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 11:48:32 -0600

    added auth checker for frontend

--- src/main/java/com/arcproject/arcproject/controllers/UserController.java
+++ src/main/java/com/arcproject/arcproject/controllers/UserController.java
@@ -1,16 +1,19 @@
 package com.arcproject.arcproject.controllers;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arcproject.arcproject.entities.UserDoc;
 import com.arcproject.arcproject.service.UserService;
+import com.arcproject.arcproject.util.CommonTools;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
     
@@ -29,4 +32,17 @@ public class UserController {
             return null;
         }
     }
+
+    @GetMapping("/user/check_logged_in")
+    public ResponseEntity<Map<String, Object>> checkLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null && authentication.isAuthenticated()) {
+        // Get user details from authentication object
+        UserDoc user = userService.getUserByEmail(authentication.getName());
+        return ResponseEntity.ok(CommonTools.convertResults(user));
+    }
+
+    return ResponseEntity.ok(CommonTools.convertResults("No user is currently authenticated"));
+}
 }
