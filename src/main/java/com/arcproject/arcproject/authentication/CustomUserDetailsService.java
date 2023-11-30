6d121b38a4e665bcb01126e28d04b6b248e1069a
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 11:44:50 -0600

    created customuserdetailsservice

--- src/main/java/com/arcproject/arcproject/authentication/CustomUserDetailsService.java
+++ src/main/java/com/arcproject/arcproject/authentication/CustomUserDetailsService.java
@@ -2,24 +2,26 @@ package com.arcproject.arcproject.authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.arcproject.arcproject.entities.UserDoc;
-import com.arcproject.arcproject.interfaces.UserInterface;
+import com.arcproject.arcproject.service.UserService;
 
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
-    private UserInterface userInterface;
+    private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserDoc user = userInterface.findByEmail(email);
+        UserDoc user = userService.getUserByEmail(email);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found");
+            throw new UsernameNotFoundException("User not found " + email + " if nothing else, email empty");
         }
 
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
