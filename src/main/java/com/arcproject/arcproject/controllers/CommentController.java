f1838212dda2e78b1acabbd74ae21293d10bba39
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 21:54:50 -0600

    added comment submission endpoint

--- src/main/java/com/arcproject/arcproject/controllers/CommentController.java
+++ src/main/java/com/arcproject/arcproject/controllers/CommentController.java
@@ -1,4 +1,5 @@
 package com.arcproject.arcproject.controllers;
+import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 
@@ -12,17 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arcproject.arcproject.entities.CommentDoc;
+import com.arcproject.arcproject.entities.StoryDoc;
+import com.arcproject.arcproject.entities.UserDoc;
 import com.arcproject.arcproject.service.CommentService;
+import com.arcproject.arcproject.service.UserService;
 import com.arcproject.arcproject.util.CommonTools;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
     private final CommentService commentService;
+    private final UserService userService;
     
     @Autowired
-    public CommentController(CommentService commentService){
+    public CommentController(CommentService commentService, UserService userService){
         this.commentService = commentService;
+        this.userService = userService;
     }
 
     @PostMapping("/display")
@@ -39,4 +45,27 @@ public class CommentController {
 
         return ResponseEntity.ok(CommonTools.convertResults(total));
     }
+
+    @PostMapping("/submit")
+    public ResponseEntity<Map<String, Object>> newStoryIntoDb(@RequestBody Map<String, String> payload, Principal principal){
+        
+        UserDoc user = userService.getUserByEmail(principal.getName());
+        
+        CommentDoc newComment = new CommentDoc();
+        newComment.setComment(payload.get("comment"));
+        newComment.setStoryUuid(payload.get("story"));
+        newComment.setAuthor(user.getuuid());
+        commentService.addComment(newComment);
+
+        return ResponseEntity.ok(CommonTools.convertResults("true"));
+    }
+
+    @PostMapping("/mycomments")
+    public ResponseEntity<Map<String, Object>> userCommentsToBrowser(Principal principal){
+
+        List<CommentDoc> comments = commentService.findByAuthorId(userService.getUserByEmail(principal.getName()).getuuid());
+
+        return ResponseEntity.ok(CommonTools.convertResults(comments));
+    }
+
 }
