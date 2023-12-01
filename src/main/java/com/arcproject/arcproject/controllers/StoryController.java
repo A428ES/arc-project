5e91cdbb64c90b145cb882a7a9e80699b149bebe
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 21:55:11 -0600

    added story submission endpoint

--- src/main/java/com/arcproject/arcproject/controllers/StoryController.java
+++ src/main/java/com/arcproject/arcproject/controllers/StoryController.java
@@ -10,16 +10,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.arcproject.arcproject.entities.StoryDoc;
+import com.arcproject.arcproject.entities.UserDoc;
 import com.arcproject.arcproject.service.StoryService;
+import com.arcproject.arcproject.service.UserService;
 import com.arcproject.arcproject.util.CommonTools;
 
 @RestController
 @RequestMapping("/")
 public class StoryController {
     private final StoryService storyService;
+    private final UserService userService;
 
     @Autowired
-    public StoryController(StoryService storyService){
+    public StoryController(UserService userService, StoryService storyService){
+        this.userService = userService;
         this.storyService = storyService;
     }
 
@@ -39,9 +43,24 @@ public class StoryController {
 
     @PostMapping("/stories/mystories")
     public ResponseEntity<Map<String, Object>> userStoriesToBrowser(Principal principal){
-        List<StoryDoc> stories = storyService.findByAuthorId(principal.getName());
+
+        List<StoryDoc> stories = storyService.findByAuthorId(userService.getUserByEmail(principal.getName()).getuuid());
 
         return ResponseEntity.ok(CommonTools.convertResults(stories));
+    }
+
+    @PostMapping("/stories/submit")
+    public ResponseEntity<Map<String, Object>> newStoryIntoDb(@RequestBody Map<String, String> payload, Principal principal){
+        
+        UserDoc user = userService.getUserByEmail(principal.getName());
+        
+        StoryDoc newStory = new StoryDoc();
+        newStory.setStory(payload.get("story"));
+        newStory.setTitle(payload.get("title"));
+        newStory.setAuthor(user.getuuid());
+        storyService.createStory(newStory);
+
+        return ResponseEntity.ok(CommonTools.convertResults("true"));
     }
 
 }
