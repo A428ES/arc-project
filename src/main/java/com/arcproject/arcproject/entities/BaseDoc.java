24caa5df0f5264370db30d032861c34667c73605
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-11-30 21:55:25 -0600

    created basedoc class

---
+++ src/main/java/com/arcproject/arcproject/entities/BaseDoc.java
@@ -0,0 +1,79 @@
+package com.arcproject.arcproject.entities;
+import org.springframework.data.annotation.Id;
+
+import com.arcproject.arcproject.util.CommonTools;
+import java.util.UUID;
+
+public abstract class BaseDoc {
+
+    @Id
+    private String id;
+
+    private String uuid;
+    private String ip_address;
+    private double created_timestamp_ms;
+    private double modified_timestamp_ms;
+    private boolean isDeleted;
+
+    public BaseDoc() {
+        if (this.uuid == null) {
+        this.uuid = UUID.randomUUID().toString();
+        double currentTimestamp = getCurrentUnixTime();
+        this.created_timestamp_ms = currentTimestamp;
+        this.modified_timestamp_ms = currentTimestamp;
+        this.isDeleted = false;
+        this.ip_address = "127.0.0.1";
+        }
+    }
+
+    private double getCurrentUnixTime() {
+        long currentTimeMillis = System.currentTimeMillis();
+        return currentTimeMillis / 1000.0; // Convert milliseconds to seconds with fractional part
+    }
+
+    // Getters
+    public String getuuid() {
+        return uuid;
+    }
+
+    public String getIpAddress() {
+        return ip_address;
+    }
+
+    public double getCreatedTimestampMs() {
+        return created_timestamp_ms;
+    }
+
+    public double getModifiedTimestampMs() {
+        return modified_timestamp_ms;
+    }
+
+    public String getDate(){
+        return CommonTools.convertStamp(this.created_timestamp_ms);
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    // Setters
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ip_address = ipAddress;
+    }
+
+    public void setCreatedTimestampMs(long createdTimestampMs) {
+        this.created_timestamp_ms = createdTimestampMs;
+    }
+
+    public void setModifiedTimestampMs(long modifiedTimestampMs) {
+        this.modified_timestamp_ms = modifiedTimestampMs;
+    }
+
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
