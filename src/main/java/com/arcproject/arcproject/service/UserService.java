ee78456f5bf15a126ba90e2570dd2dad00c78e80
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-01 09:30:13 -0600

    created method to deliver user with stats on comments/stories

--- src/main/java/com/arcproject/arcproject/service/UserService.java
+++ src/main/java/com/arcproject/arcproject/service/UserService.java
@@ -4,17 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arcproject.arcproject.entities.UserDoc;
+import com.arcproject.arcproject.interfaces.CommentInterface;
+import com.arcproject.arcproject.interfaces.StoryInterface;
 import com.arcproject.arcproject.interfaces.UserInterface;
 import com.arcproject.arcproject.model.UserRegistration;
+import com.arcproject.arcproject.util.CommonTools;
 import com.arcproject.arcproject.util.SecurityUtil;
 
 @Service
 public class UserService {
     private final UserInterface userInterface;
+    private final StoryInterface storyInterface;
+    private final CommentInterface commentInterface;
 
     @Autowired
-    public UserService(UserInterface userInterface){
+    public UserService(UserInterface userInterface, CommentInterface commentInterface, StoryInterface storyInterface){
         this.userInterface = userInterface;
+        this.storyInterface = storyInterface;
+        this.commentInterface = commentInterface;
     }
 
     public UserDoc getUserByEmail(String userEmail) {
@@ -38,5 +45,15 @@ public class UserService {
 
     public void newUser(UserDoc newUser){
         userInterface.save(newUser);
+    }
+
+    public UserDoc userWithStats(String userEmail){
+        UserDoc userDoc = userInterface.findByEmail(userEmail);
+
+        userDoc.setStoryCount(storyInterface.searchByAuthorUuid(userDoc.getuuid()).size());
+        userDoc.setCommentCount(commentInterface.findByAuthorId(userDoc.getuuid()).size());
+        userDoc.setCreated(CommonTools.convertStamp(userDoc.getCreatedTimestampMs()));
+
+        return userDoc;
     }
 }
